c246ac6bdf1f35757ebbb17dbc0f11ae8c0d839d
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-10-06 11:54:19 +0530

    team images added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer } from "./common/Footer";
 import { FirstBanner } from "./components/FirstBanner";
+import { OurTeam } from "./components/OurTeam";
 import { SecondBanner } from "./components/SecondBanner";
 import { ServicesSection } from "./components/ServicesSection";
 import { Testimonial } from "./components/Testimonial";
@@ -14,6 +15,7 @@ function App() {
       <ServicesSection />
       <WhyUsSection />
       <Testimonial />
+      <OurTeam />
       <Footer />
     </div>
   );
